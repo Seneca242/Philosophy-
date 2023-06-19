cabf93385d68df0274c07091e8f82d2514afef20
Seneca242 <flabbergasted242@gmail.com>
2023-06-19 12:22:29 +0300

    made image in MovementPhilosophersCollectionViewController round

--- Philosophy!/Views/PhilosophicalWithImageCollectionViewCell.swift
+++ Philosophy!/Views/PhilosophicalWithImageCollectionViewCell.swift
@@ -8,52 +8,52 @@
 import UIKit
 
 class PhilosophicalWithImageCollectionViewCell: UICollectionViewCell {
-    
+
     var alignment: NSTextAlignment = .center {
         didSet {
             titleLabel.textAlignment = alignment
         }
     }
-    
-    private let titleLabel: UILabel = {
+
+    private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
         label.numberOfLines = 0
         return label
     }()
-    
+
     private lazy var philosopherImage: UIImageView = {
         let image = UIImageView()
         return image
     }()
-    
+
+    private var isConfigured = false
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupConstraints()
     }
-    
-    override func layoutIfNeeded() {
-        super.layoutIfNeeded()
-        philosopherImage.layer.cornerRadius = philosopherImage.frame.width / 2
-    }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     func configureCell(with philosopher: Philosopher) {
+        layoutIfNeeded()
         titleLabel.text = philosopher.name
+        philosopherImage.layer.cornerRadius = philosopherImage.bounds.width / 2
         philosopherImage.image = UIImage(named: "\(philosopher.philosopherImage)")
         philosopherImage.backgroundColor = .black
-//        philosopherImage.layer.cornerRadius = philosopherImage.frame.width / 2
+        philosopherImage.layer.cornerRadius = philosopherImage.frame.width / 2
         philosopherImage.contentMode = .scaleAspectFill
         philosopherImage.clipsToBounds = true
-        }
-    
+        philosopherImage.layer.masksToBounds = true
+    }
+
     private func setupConstraints() {
         contentView.addSubview(philosopherImage)
         contentView.addSubview(titleLabel)
-        
+
         philosopherImage.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             philosopherImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
@@ -61,7 +61,7 @@ class PhilosophicalWithImageCollectionViewCell: UICollectionViewCell {
             philosopherImage.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.8),
             philosopherImage.widthAnchor.constraint(equalTo: philosopherImage.heightAnchor)
         ])
-        
+
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             titleLabel.leadingAnchor.constraint(equalTo: philosopherImage.trailingAnchor, constant: 10),
